107e0342da564ed27c08a108a6d00e052cc3a145
Thad Guidry <thadguidry@gmail.com>
2024-06-26 14:14:14 +0800

    also try to account for QIDs within text of an HTML element start/end brackets

--- linker.js
+++ linker.js
@@ -1,4 +1,4 @@
-const regex = /(?<=[ "'(>])Q\d+(?=[ "').<])/g;
+const regex = /(?<=>|^|[\^\s("]+)Q\d+(?=[$\s\w)"]*<)/g;
 
 document.body.innerHTML = document.body.innerHTML.replace(regex, (match) => {
   return `<a href="https://wikidata.org/entity/${match}" target="_blank">${match}</a>`;
