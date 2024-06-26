5c94d5d685cc8ce14abadd0178acdd5accdd0d70
Thad Guidry <thadguidry@gmail.com>
2024-06-26 10:04:24 +0800

    use positive lookbehind and lookahead for likely formats wrapping QIDs Fixes #1

--- linker.js
+++ linker.js
@@ -1,5 +1,5 @@
-const regex = /([\s"'])(Q\d+)/g;
+const regex = /(?<=[ "'(>])Q\d+(?=[ "').<])/g;
 
 document.body.innerHTML = document.body.innerHTML.replace(regex, (match) => {
-  return '$1<a href="https://wikidata.org/entity/$2" target="_blank">$2</a>';
+  return `<a href="https://wikidata.org/entity/${match}" target="_blank">${match}</a>`;
 });
